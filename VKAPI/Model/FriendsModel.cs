59b7843a7512d34b8d7aa5129f3e745827089ba1
Alex493049469 <alex493049469@yandex.ru>
2015-04-17 19:45:21 +0700

    vk api

---
+++ VKAPI/Model/FriendsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace VKAPI.Model
+{
+    [XmlRoot("response")]
+    public class FriendsModel
+    {
+
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("items")]
+        [XmlArrayItem("user", typeof(User))]
+        public List<User> Items { get; set; }
+
+    }
+}
