ac12db49a91ff4df46b239944876b92b6d6347ba
Alex493049469 <alex493049469@yandex.ru>
2015-06-25 22:57:18 +0700

    оптимизация работы с API

--- VKAPI/VkAudio.cs
+++ VKAPI/VkAudio.cs
@@ -11,28 +11,9 @@ namespace VKAPI
     /// <summary>
     ///     Класс для работы с аудиозаписями
     /// </summary>
-    public class VkAudio
+    public class VkAudio : VkBase
     {
-        //формируемая строка необходимых пареметров
-        private string _url;
-
-        private string Url
-        {
-            get { return _url; }
-            set
-            {
-                if (!string.IsNullOrEmpty(_url))
-                {
-                    _url += "&" + value;
-                }
-                else
-                {
-                    
-                    _url += value;
-                }
-            }
-        }
-
+ 
         /// <summary>
         ///    Возвращает список аудиозаписей пользователя или сообщества
         /// </summary>
@@ -47,50 +28,25 @@ namespace VKAPI
         /// <returns></returns>
         public AudioModel Get(int ownerId, int albumId, string audioIds, int offset , int count)
         {
-            _url = null;
-
-            if (ownerId > 0)
-            {
-                Url = "owner_id=" + ownerId;
-            }
-            if (albumId > 0)
-            {
-                Url = "album_id=" + albumId;
-            }
-            if (!string.IsNullOrEmpty(audioIds))
-            {
-                Url = "audio_ids=" + audioIds;
-            }
-            if (offset > 0)
-            {
-                Url = "offset=" + offset;
-            }
-            if (count > 0)
-            {
-                Url = "count=" + count;
-            }
-           
-            var reqGet =
-                WebRequest.Create(@"https://api.vk.com/method/audio.get?" + Url +
-                                  "&v=5.29&access_token=" + VkMain.token);
-            var resp = reqGet.GetResponse();
-            var stream = resp.GetResponseStream();
-            var sr = new StreamReader(stream);
-            var s = sr.ReadToEnd();
-
+            //используемый метод
+            Method = "audio.get";
+            ClearParameters();
+            //добавляем параметры
+            AddParameter("owner_id=", ownerId);
+            AddParameter("album_id=", albumId);
+            AddParameter("audio_ids=", audioIds);
+            AddParameter("offset=", offset);
+            AddParameter("count=", count);
+            //получаем данные в json
+            string str = GetData();
             //десериализуем
-            var audioModel = JsonConvert.DeserializeObject<AudioModel>(s);
-
+            var audioModel = JsonConvert.DeserializeObject<AudioModel>(str);
             return audioModel;
         }
 
-        public Task<AudioModel> GetAsync(int ownerId = 0, int albumId = 0, string audioIds = null,  int offset = 0, int count =1000)
+        public Task<AudioModel> GetAsync(int ownerId = 0, int albumId = 0, string audioIds = "sdfsdf",  int offset = 0, int count =1000)
         {
-            return Task.Run(() =>
-            {
-                var audioModel = Get(ownerId, albumId, audioIds, offset, count);
-                return audioModel;
-            });
+            return Task.Run(() => Get(ownerId, albumId, audioIds, offset, count));
         }
 
         /// <summary>
@@ -105,112 +61,78 @@ namespace VKAPI
         /// <param name="offset">смещение, необходимое для выборки определенного подмножетсва аудиозаписей. По умолчанию — 0. </param>
         /// <param name="count">количество аудиозаписей, информацию о которых необходимо вернуть. максимальное значение 300</param>
         /// <returns></returns>
-        public AudioModel Search(string q, short autoComplete, short lyrics, short performerOnly, short sort, short searchOwn, int offset, int count)
+        public AudioModel Search(string q, int autoComplete, int lyrics, int performerOnly, int sort, int searchOwn, int offset, int count)
         {
-            _url = null;
-
-            if (!string.IsNullOrEmpty(q))
-            {
-                Url = "q=" + q;
-            }
-            if (autoComplete > 0)
-            {
-                Url = "auto_complete=" + autoComplete;
-            }
-            if (lyrics > 0)
-            {
-                Url = "lyrics=" + lyrics;
-            }
-            if (performerOnly > 0)
-            {
-                Url = "performer_only=" + performerOnly;
-            }
-            if (sort > 0)
-            {
-                Url = "sort=" + sort;
-            }
-            if (searchOwn > 0)
-            {
-                Url = "search_own=" + searchOwn;
-            }
-            if (offset > 0)
-            {
-                Url = "offset=" + offset;
-            }
-            if (count > 0)
-            {
-                Url = "count=" + count;
-            }
-
-
-            var reqGet =
-                WebRequest.Create(@"https://api.vk.com/method/audio.search?" + Url +
-                                  "&v=5.29&access_token=" + VkMain.token);
-            var resp = reqGet.GetResponse();
-            var stream = resp.GetResponseStream();
-            var sr = new StreamReader(stream);
-            var s = sr.ReadToEnd();
-
+            //используемый метод
+            Method = "audio.search";
+            ClearParameters();
+            //добавляем параметры
+            AddParameter("q=", q);
+            AddParameter("auto_complete=", autoComplete);
+            AddParameter("lyrics=", lyrics);
+            AddParameter("performer_only=", performerOnly);
+            AddParameter("sort=", sort);
+            AddParameter("search_own=", searchOwn);
+            AddParameter("offset=", offset);
+            AddParameter("count=", count);
+            //получаем данные в json
+            string str = GetData();
             //десериализуем
-            var audioModel = JsonConvert.DeserializeObject<AudioModel>(s);
-
+            var audioModel = JsonConvert.DeserializeObject<AudioModel>(str);
             return audioModel;
+          
         }
 
         public Task<AudioModel> SearchAsync(string q, short autoComplete=1, short lyrics = 0, short performerOnly =0, short sort =0, short searchOwn = 1, int offset =0, int count=300)
         {
-            return Task.Run(() =>
-            {
-                var audioModel = Search(q, autoComplete, lyrics, performerOnly, sort, searchOwn, offset, count);
-                return audioModel;
-            });
+            return Task.Run(() => Search(q, autoComplete, lyrics, performerOnly, sort, searchOwn, offset, count));
+        }
+
+       
+        /// <summary>
+        ///  Копирует аудиозапись на страницу пользователя
+        /// </summary>
+        /// <param name="audioId">Идентификатор аудиозаписи</param>
+        /// <param name="ownerId">идентификатор владельца аудиозаписи (пользователь или сообщество). </param>
+        public void Add(int audioId, int ownerId)
+        {
+            //используемый метод
+            Method = "audio.add";
+            ClearParameters();
+            //добавляем параметры
+            AddParameter("audio_id=", audioId);
+            AddParameter("owner_id=", ownerId);
+
+            //получаем данные в json
+            GetData();
+        }
+
+        public Task AddAsync(int audioId, int ownerId)
+        {
+            return Task.Run(() => { Add(audioId, ownerId); });
         }
 
         /// <summary>
-        ///     Копирует аудиозапись на страницу пользователя
+        /// Удаляет аудиозапись со страницы пользователя
         /// </summary>
-        /// <param name="countAudio"></param>
-        /// <returns></returns>
-        public void Add(int audio_id, int owner_id)
+        /// <param name="audioId"></param>
+        /// <param name="ownerId"></param>
+        public void Delete(int audioId, int ownerId)
         {
-            var reqGET =
-                WebRequest.Create(@"https://api.vk.com/method/audio.add?audio_id=" + audio_id + "&owner_id=" + owner_id +
-                                  "&v=5.29&access_token=" + VkMain.token);
-            var resp = reqGET.GetResponse();
-            var stream = resp.GetResponseStream();
-            var sr = new StreamReader(stream);
-            var s = sr.ReadToEnd();
+            //используемый метод
+            Method = "audio.delete";
+            ClearParameters();
+            //добавляем параметры
+            AddParameter("audio_id=", audioId);
+            AddParameter("owner_id=", ownerId);
+
+            //получаем данные в json
+            GetData();
         }
 
-        public Task AddAsync(int audio_id, int owner_id)
+        public Task DeleteAsync(int audioId, int ownerId)
         {
-            return Task.Run(() => { Add(audio_id, owner_id); });
-        }
-
-        /// <summary>
-        ///   Удаляет аудиозапись со страницы пользователя
-        /// </summary>
-        /// <param name="countAudio"></param>
-        /// <returns></returns>
-        public void Delete(int audio_id, int owner_id)
-        {
-            var reqGET =
-                WebRequest.Create(@"https://api.vk.com/method/audio.delete?audio_id=" + audio_id + "&owner_id=" +
-                                  owner_id + "&v=5.29&access_token=" + VkMain.token);
-            var resp = reqGET.GetResponse();
-            var stream = resp.GetResponseStream();
-            var sr = new StreamReader(stream);
-            var s = sr.ReadToEnd();
-        }
-
-        /// <summary>
-        ///    Удаляет аудиозапись со страницы пользователя
-        /// </summary>
-        /// <param name="countAudio"></param>
-        /// <returns></returns>
-        public Task DeleteAsync(int audio_id, int owner_id)
-        {
-            return Task.Run(() => { Delete(audio_id, owner_id); });
+            return Task.Run(() => { Delete(audioId, ownerId); });
         }
     }
 }
---
+++ VKAPI/VkBase.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VKAPI
+{
+    public abstract class VkBase
+    {
+        //базовый Url
+        private string _baseUrl = @"https://api.vk.com/method/";
+        ///вызываемый метод
+        protected string Method;
+        //список необходимых полей
+        protected string Fields;
+        //список необходимых параметров параметр = значение
+        protected Dictionary<string, string> Parameters = new Dictionary<string, string>();
+        //версия Api
+        protected string ApiVersion = "v=5.29";
+        //Сфомированная строка запроса
+        protected string Url;
+
+        /// <summary>
+        /// Отправка запроса и получение ответа в строку
+        /// </summary>
+        /// <returns></returns>
+        protected string GetData()
+        {
+            Url = GenerateRequest();
+
+            var reqGet = WebRequest.Create(Url);
+            var resp = reqGet.GetResponse();
+            var stream = resp.GetResponseStream();
+            var sr = new StreamReader(stream);
+            var str = sr.ReadToEnd();
+
+            //возвращаем данные
+            return str;
+        }
+
+        /// <summary>
+        /// склеивание всех параметров в строку запроса
+        /// </summary>
+        /// <returns></returns>
+        public string GenerateRequest()
+        {
+            string url = _baseUrl + Method + "?";
+
+            if (Parameters.Count > 0)
+            {
+                url += ConvertToStringParameters();
+            }
+
+            if (!string.IsNullOrEmpty(Fields))
+            {
+                url += "&" + Fields;
+            }
+
+            url += "&" + ApiVersion + "&access_token=" + VkMain.token;
+
+
+            return url;
+        }
+
+        /// <summary>
+        /// Добавление параметров, параметры = 0 или "" не добавляются
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <param name="value"></param>
+        protected void AddParameter(string parameter, object value)
+        {
+            if (value != null)
+            {
+                Type type = value.GetType();
+
+                if (type.Name == "Int32")
+                {
+                    int val = Convert.ToInt32(value);
+                    if (val > 0)
+                    {
+                        Parameters.Add(parameter, value.ToString());
+                    }
+                }
+
+                if (type.Name == "String")
+                {
+                    string val = Convert.ToString(value);
+                    if (val.Length > 0)
+                    {
+                        Parameters.Add(parameter, value.ToString());
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Удаление параметров
+        /// </summary>
+        protected void ClearParameters()
+        {
+            Parameters.Clear();
+        }
+
+        /// <summary>
+        /// Преобразование списка параметров в строку
+        /// </summary>
+        /// <returns></returns>
+        private string ConvertToStringParameters()
+        {
+            string param = "";
+
+            foreach (var item in Parameters)
+            {
+                if (param.Length > 0)
+                {
+                    param += "&" + item.Key + item.Value;
+                }
+                else
+                {
+                    param = item.Key + item.Value;
+                }
+            }
+
+            return param;
+        }
+
+
+    }
+}
